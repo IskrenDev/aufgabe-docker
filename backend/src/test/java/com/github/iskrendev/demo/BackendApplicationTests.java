9b58e43b2ee54a9573706e516427d3d20b8bcf51
iskrenradev <iskrenr005@gmail.com>
2023-11-03 10:16:22 +0100

    Add backend module

---
+++ backend/src/test/java/com/github/iskrendev/demo/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.iskrendev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
